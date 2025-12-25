c1184bd120636c507d90e840d1eaf317a8bcc857
dstogi <dstogi@web.de>
2025-12-26 00:05:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
- registerType: 'autoUpdate',
+  registerType: 'autoUpdate',
   includeAssets: [
     'apple-touch-icon.png',
     'pwa-192.png',
